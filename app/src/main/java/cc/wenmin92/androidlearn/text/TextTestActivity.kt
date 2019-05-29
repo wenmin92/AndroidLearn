68b1456633e80bf3068932583dad16b3ca8d1b07
wenmin92 <wenmin92@gmail.com>
2019-05-29 13:57:43 +0800

    [add] android.text UML

--- app/src/main/java/cc/wenmin92/androidlearn/text/TextTestActivity.kt
+++ app/src/main/java/cc/wenmin92/androidlearn/text/TextTestActivity.kt
@@ -1,10 +1,16 @@
 package cc.wenmin92.androidlearn.text
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputFilter
+import android.text.Editable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import cc.wenmin92.androidlearn.R
+import timber.log.Timber
+
 
 class TextTestActivity : AppCompatActivity() {
 
@@ -13,6 +19,31 @@ class TextTestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_text_test)
 
         val etText = findViewById<EditText>(R.id.et_text)
-        etText.filters = arrayOf(InputFilter.LengthFilter(4))
+        // etText.filters = arrayOf(InputFilter.LengthFilter(4))
+
+        val watcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                Timber.d(s.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Timber.d("onTextChanged, s=$s")
+            }
+
+        }
+
+        val spannable = SpannableString("This is a multiline text. LineBackgroundSpan is applied here. This is a multiline text.")
+        spannable.setSpan(watcher, 1, 5, Spanned.SPAN_MARK_POINT)
+
+        etText.setText(spannable)
+
+        val tvText = findViewById<TextView>(R.id.tv_text)
+        val text = "This is a multiline text. LineBackgroundSpan is applied here. This is a multiline text."
+        val string = SpannableString(text)
+        // string.setSpan(LineBackgroundSpan.Standard(Color.YELLOW), 26, 61, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tvText.text = string;
     }
 }
