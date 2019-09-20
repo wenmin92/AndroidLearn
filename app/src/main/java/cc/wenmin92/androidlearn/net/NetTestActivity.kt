7c173c88959253221ee3522a7e58b3bed62c6cb8
wenmin92 <wenmin92@gmail.com>
2019-09-20 18:55:16 +0800

    [*] net base

--- app/src/main/java/cc/wenmin92/androidlearn/net/NetTestActivity.kt
+++ app/src/main/java/cc/wenmin92/androidlearn/net/NetTestActivity.kt
@@ -1,10 +1,10 @@
 package cc.wenmin92.androidlearn.net
 
+import android.content.BroadcastReceiver
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.LinkProperties
-import android.net.Network
-import android.net.NetworkCapabilities
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.*
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -21,38 +21,81 @@ class NetTestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_net_test)
 
         val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        cm.addDefaultNetworkActiveListener {
-            log("onNetworkActive")
+
+        // active network
+        log("$TAG activeNetworkInfo: ${cm.activeNetworkInfo}")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            log("$TAG activeNetwork: ${cm.activeNetwork}")
         }
+        cm.addDefaultNetworkActiveListener { log("onNetworkActive") }
         Timber.d("$TAG isDefaultNetworkActive: ${cm.isDefaultNetworkActive}")
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            cm.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-                override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
-                    log("onCapabilitiesChanged: network[$network], networkCapabilities[$networkCapabilities]")
-                }
+        log("$TAG allNetworks: ${cm.allNetworks.joinToString()}")
+        log("$TAG allNetworkInfo: ${cm.allNetworkInfo.joinToString()}")
 
-                override fun onLost(network: Network?) {
-                    log("onLost")
-                }
 
-                override fun onLinkPropertiesChanged(network: Network?, linkProperties: LinkProperties?) {
-                    log("onLinkPropertiesChanged")
-                }
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        //     cm.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+        //         override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
+        //             log("onCapabilitiesChanged: network[$network], networkCapabilities[$networkCapabilities]")
+        //         }
+        //
+        //         override fun onLost(network: Network?) {
+        //             log("onLost: network[$network]")
+        //         }
+        //
+        //         override fun onLinkPropertiesChanged(network: Network?, linkProperties: LinkProperties?) {
+        //             log("onLinkPropertiesChanged: network[$network], linkProperties[$linkProperties]")
+        //         }
+        //
+        //         override fun onUnavailable() {
+        //             log("onLinkPropertiesChanged")
+        //         }
+        //
+        //         override fun onLosing(network: Network?, maxMsToLive: Int) {
+        //             log("onLosing: network[$network], maxMsToLive[$maxMsToLive]")
+        //         }
+        //
+        //         override fun onAvailable(network: Network?) {
+        //             log("onLosing: network[$network]")
+        //         }
+        //     })
+        // }
+        val request = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+        cm.registerNetworkCallback(request, object : ConnectivityManager.NetworkCallback() {
+            override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
+                log("onCapabilitiesChanged: network[$network], networkCapabilities[$networkCapabilities]")
+            }
 
-                override fun onUnavailable() {
-                    log("onLinkPropertiesChanged")
-                }
+            override fun onLost(network: Network?) {
+                log("onLost: network[$network]")
+            }
 
-                override fun onLosing(network: Network?, maxMsToLive: Int) {
-                    log("onLosing")
-                }
+            override fun onLinkPropertiesChanged(network: Network?, linkProperties: LinkProperties?) {
+                log("onLinkPropertiesChanged: network[$network], linkProperties[$linkProperties]")
+            }
 
-                override fun onAvailable(network: Network?) {
-                    log("onLosing")
-                }
-            })
-        }
+            override fun onUnavailable() {
+                log("onLinkPropertiesChanged")
+            }
+
+            override fun onLosing(network: Network?, maxMsToLive: Int) {
+                log("onLosing: network[$network], maxMsToLive[$maxMsToLive]")
+            }
+
+            override fun onAvailable(network: Network?) {
+                log("onLosing: network[$network]")
+            }
+        })
+
+        val filter = IntentFilter().apply {  }
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+
+            }
+        }, filter)
     }
 
     fun log(msg: String) {
